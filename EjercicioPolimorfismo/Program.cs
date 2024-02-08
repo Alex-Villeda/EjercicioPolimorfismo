ef72fb36f80cec914c039bb8217b7f9bc5e9e5c7
Alex-Villeda <152237578+Alex-Villeda@users.noreply.github.com>
2024-02-07 18:54:40 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioPolimorfismo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Figura cuadrado = new Cuadrado(5);
+        Figura circulo = new Circulo(3);
+
+        MostrarArea(cuadrado);
+        MostrarArea(circulo);
+    }
+
+    static void MostrarArea(Figura figura)
+    {
+        Console.WriteLine($"Área de la figura: {figura.CalcularArea()}");
+    }
+}
+
+abstract class Figura
+{
+    public abstract double CalcularArea();
+}
+
+class Cuadrado : Figura
+{
+    private double lado;
+
+    public Cuadrado(double lado)
+    {
+        this.lado = lado;
+    }
+
+    public override double CalcularArea()
+    {
+        return lado * lado;
+    }
+}
+
+class Circulo : Figura
+{
+    private double radio;
+
+    public Circulo(double radio)
+    {
+        this.radio = radio;
+    }
+
+    public override double CalcularArea()
+    {
+        return Math.PI * radio * radio;
+    }
+}
